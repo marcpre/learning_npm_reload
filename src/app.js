369405bb5750e62c9e38f46bdf6617a9a4353395
marcpre <marcus.presich@gmail.com>
2017-11-27 05:45:22 +0000

    linting

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path = require('path')
-var reload = require('reload')
+const reload = require('reload')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
 const app = express()
 
 const publicDir = path.join(__dirname, '/views')
 
 app.use(express.static(path.join(__dirname, '/../node_modules'))) // public folder!
+app.use(logger(process.env.LOG_ENV))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false,
+}))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(publicDir, 'index.html'))
@@ -15,8 +23,8 @@ app.get('/', (req, res) => {
 const port = process.env.APP_PORT || 8080
 const host = process.env.APP_HOST || 'localhost'
 
-// Reload code here 
-reload(app);
+// Reload code here
+reload(app)
 
 app.listen(port, () => {
   console.log(`Listening on ${host}:${port}`)
